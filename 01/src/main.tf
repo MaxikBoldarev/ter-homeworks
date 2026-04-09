2b1f20e774b2dfc392133740593d636c9ce7f01d
maxik1394 <maxikboldarev13@yandex.ru>
2026-04-09 12:35:42 +0200

    terraform

---
+++ 01/src/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+    }
+  }
+  required_version = "=1.14.8" /*Многострочный комментарий.
+ Требуемая версия terraform */
+}
+provider "docker" {}
+
+#однострочный комментарий
+
+resource "random_password" "random_string" {
+  length      = 16
+  special     = false
+  min_upper   = 1
+  min_lower   = 1
+  min_numeric = 1
+}
+
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = true
+}
+
+resource "docker_container" "nginx" {                          
+  image = docker_image.nginx.image_id
+  name  = "hello_world"
+
+# 1. Отсутствует имя ресурса
+# 2. Имя начинается с цифры
+# 3. random_password было указано не верное емя ресурса
+
+  ports {
+    internal = 80
+    external = 9090
+  }
+}
+
